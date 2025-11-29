cd9aab28890ee7181a8c1076a0a66fd8f25ca8b8
ytalgocrack-svg <ytalgocrack@gmail.com>
2025-11-29 17:25:56 +0530

    Update tailwind.config.js

--- LogoForge Web APP/tailwind.config.js
+++ LogoForge Web APP/tailwind.config.js
@@ -4,8 +4,27 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+  theme: 
+  /** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
   theme: {
+    extend: {
+      colors: {
+        // This magic line connects Tailwind to your CSS variable
+        primary: 'rgb(var(--primary) / <alpha-value>)', 
+      },
+    },
+  },
+  plugins: [],
+}
+  {
     extend: {},
   },
   plugins: [],
+
 }
